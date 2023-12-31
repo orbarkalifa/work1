207838befc0ac251e1c93df53f6ab8157a7ce7f8
FradkinDaniel <143734746+FradkinDaniel@users.noreply.github.com>
2023-12-31 11:07:54 +0200

    Update Main.java
    Comments

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File report = new File("report.txt");
-        File dep = new File("CS.txt");
-        File course = new File("hedva.txt");
-        PrintWriter writer = new PrintWriter(new FileWriter(report));
+        //open files and inputs
+        File report = new File("report.txt");// new file for output
+        File dep = new File("CS.txt"); // file sorted by id format firstname lastname id
+        File course = new File("hedva.txt"); // file sorted by id format id g1 g2
+        PrintWriter writer = new PrintWriter(new FileWriter(report));// open write in output
         Scanner depInput = new Scanner(dep);
         Scanner courseInput = new Scanner(course);
-
+        // vars for data if needed(names) and id for comper 
         String fName = depInput.next();
         String lName = depInput.next();
         int depID = depInput.nextInt();
@@ -21,16 +22,16 @@ public class Main {
 
         while (depInput.hasNext() && courseInput.hasNext()) {
 
-            if (depID < courseID) {
+            if (depID < courseID) { //id in course is higher so we read in dep
                 fName = depInput.next();
                 lName = depInput.next();
                 depID = depInput.nextInt();
 
-            } else if (depID > courseID) {
+            } else if (depID > courseID) {//id in dep is higher we read in course
                 courseInput.nextLine();
                 if (courseInput.hasNext()) courseID = courseInput.nextInt();
 
-            } else {
+            } else {//match, we advance dep more convinent 
                 writer.println(depID + " " + fName + " " + lName + " " + (courseInput.nextInt() + courseInput.nextInt())/2);
                 fName = depInput.next();
                 lName = depInput.next();
@@ -38,7 +39,7 @@ public class Main {
 
             }
         }
-
+        //end code
         writer.close();
         depInput.close();
         courseInput.close();
